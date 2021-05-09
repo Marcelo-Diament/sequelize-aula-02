fb9619496ce50f6a9a2181c361ba35dc3a395da5
Marcelo-Diament <marcelodiament@gmail.com>
2021-05-09 19:37:11 -0300

    feature: clean users controller

--- backend/controllers/users.js
+++ backend/controllers/users.js
@@ -1,24 +1,4 @@
-const Sequelize = require('sequelize'),
-  config = require('../config/database'),
-  db = new Sequelize(config),
-  { Usuario } = require('../models')
-
-const users = [{
-  id: 1,
-  nome: 'Fulano',
-  sobrenome: 'de Tal',
-  email: 'fulano@detal.com',
-  senha: 'Senha123',
-  id_funcao: 2
-},
-{
-  id: 2,
-  nome: 'Ciclano',
-  sobrenome: 'Tal Qual',
-  email: 'ciclano@talqual.com',
-  senha: 'Senha123',
-  id_funcao: 1
-}]
+const { Usuario } = require('../models')
 
 const controller = {
   list: async (req, res, next) => {
