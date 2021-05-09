4a9039f90d434826b8bef5680847c14a1feea2e2
Marcelo-Diament <marcelodiament@gmail.com>
2021-05-09 01:34:01 -0300

    feature: user route and controller

--- backend/controllers/users.js
+++ backend/controllers/users.js
@@ -1,24 +1,34 @@
+const users = [{
+  id: 1,
+  nome: 'Fulano',
+  sobrenome: 'de Tal',
+  email: 'fulano@detal.com',
+  senha: 'Senha123',
+  id_funcao: 2
+},
+{
+  id: 2,
+  nome: 'Ciclano',
+  sobrenome: 'Tal Qual',
+  email: 'ciclano@talqual.com',
+  senha: 'Senha123',
+  id_funcao: 1
+}]
+
 const controller = {
   list: (req, res, next) => {
     res.render('users', {
       title: 'Página de Usuários',
       subtitle: 'Confira a seguir os usuários cadastrados em nosso banco de dados',
-      users: [{
-        id: 1,
-        nome: 'Fulano',
-        sobrenome: 'de Tal',
-        email: 'fulano@detal.com',
-        senha: 'Senha123',
-        id_funcao: 2
-      },
-      {
-        id: 2,
-        nome: 'Ciclano',
-        sobrenome: 'Tal Qual',
-        email: 'ciclano@talqual.com',
-        senha: 'Senha123',
-        id_funcao: 1
-      }]
+      users
+    })
+  },
+  index: (req, res, next) => {
+    const { id } = req.params
+    res.render('users', {
+      title: 'Página de Usuário',
+      subtitle: `Confira a seguir o usuário de id ${id}`,
+      users: [users[id - 1]]
     })
   }
 }
--- backend/routes/users.js
+++ backend/routes/users.js
@@ -2,6 +2,7 @@ const express = require('express'),
   router = express.Router(),
   controller = require('../controllers/users')
 
+router.get('/:id', controller.index)
 router.get('/', controller.list)
 
 module.exports = router
