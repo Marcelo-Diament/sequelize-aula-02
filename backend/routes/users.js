19016776e883890f584659aac289b1b50f0b9eaf
Marcelo-Diament <marcelodiament@gmail.com>
2021-05-09 20:08:54 -0300

    feature: search route

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -2,6 +2,7 @@ const express = require('express'),
   router = express.Router(),
   controller = require('../controllers/users')
 
+router.get('/search/:searchParam/:searchValue', controller.search)
 router.get('/add', controller.addUser)
 router.post('/:id/delete', controller.delete)
 router.get('/:id', controller.index)
